6e1fd1a0a64f5808336ab50370b9a2b0cf962410
Arif6474 <arifulislam64743@gmail.com>
2022-02-27 15:12:43 +0600

    mealdb changed

--- js/mealdb.js
+++ js/mealdb.js
@@ -2,20 +2,40 @@
 const searchFood = () =>{
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
+    const error = document.getElementById('error');
+    console.log(error);
     // console.log(searchText);
 
-    searchField.value = '';
+    if (searchText == '' ){
+        // alert('please enter a number')
+        searchField.value = '';
+        
+        error.innerText = 'No result found'
+        searchText = '';
+    }else if (searchText <= 0){
+        searchField.value = '';
+        error.innerText = 'Please give valid information'
+        
+    } else{
+        searchField.value = '';
     const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
     // console.log(url);
 
     fetch(url)
     .then(res => res.json())
     .then(data => displaySearchResult(data.meals));
+    error.innerText = 'No result found'
+    
+
+    }
+
+    
 
 }
 const displaySearchResult = meals =>{
        const searchResult = document.getElementById('search-result');
-       meals.forEach(meal =>{
+       searchResult.textContent = '';
+       meals.forEach (meal =>{
         // console.log(meals);
         const div =document.createElement('div');
         div.classList.add('col');
